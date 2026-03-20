d3c0caa75ead80bad4a74e347b49dcdbc7f39c9b
Branislav Đalić <branislav.djalic@gmail.com>
2026-03-20 02:54:11 +0100

    socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,12 @@ import EmailIcon from '@mui/icons-material/Email';
 import YTIcon from '@mui/icons-material/YouTube';
 import '../assets/styles/Footer.scss'
 
+const FarcasterIcon = () => (
+  <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" style={{verticalAlign: 'middle'}}>
+    <path d="M5.3 2h13.4l1.8 5.4h1.2v10.8c0 .6-.2 1-.5 1.4-.4.3-.8.5-1.4.5h-.7c-.5 0-1-.2-1.3-.5-.4-.4-.5-.8-.5-1.4v-5.4h-1.2L15 8.6l-1 3.7h-1.1v5.4c0 .6-.2 1-.5 1.4-.4.3-.8.5-1.4.5h-.7c-.5 0-1-.2-1.3-.5-.4-.4-.5-.8-.5-1.4v-5.4H7.2L6.1 8.6l-1 3.7H3.8V7.4h1.2L5.3 2z"/>
+  </svg>
+);
+
 function Footer() {
   return (
     <footer>
@@ -15,6 +21,7 @@ function Footer() {
             <a href="https://x.com/LordOfThePies4" target="_blank" rel="noreferrer"><TwitterIcon/></a>
             <a href="https://www.youtube.com/@interlooper" target="_blank" rel="noreferrer"><YTIcon/></a>
             <a href="mailto:branislav.djalic@gmail.com" target="_blank" rel="noreferrer"><EmailIcon/></a>
+            <a href="https://farcaster.xyz/bane84" target="_blank" rel="noreferrer"><FarcasterIcon/></a>
       </div>
       <p></p>
     </footer>
